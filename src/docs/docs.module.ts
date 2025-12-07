582b9e809fb660fbba6ea10e2b31ba65150e2fb4
HenryCortez <henrystevencortez4566@gmail.com>
2025-12-06 21:54:58 -0500

    cosas

--- src/docs/docs.module.ts
+++ src/docs/docs.module.ts
@@ -3,11 +3,12 @@ import { EncryptionModule } from './encryption/encryption.module'
 import { DocsController } from './docs.controller'
 import { EncryptionService } from './encryption/encryption.service'
 import { CommonModule } from '../common/common.module'
+import { DocsService } from './docs.service';
 
 @Module({
   imports: [EncryptionModule, CommonModule],
   controllers: [DocsController],
-  providers: [EncryptionService],
+  providers: [EncryptionService, DocsService],
   exports: [EncryptionModule]
 })
 export class DocsModule {}
---
+++ src/docs/docs.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+
+@Injectable()
+export class DocsService {
+    constructor(
+        private readonly prisma: PrismaService
+    ) {}
+
+    async saveDocumentOnCloud(document: any) {
+        let url;
+        return url;
+    }
+
+    async createDocument(dto: any){
+        
+    }
+}
